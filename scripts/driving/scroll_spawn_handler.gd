8aa45d9d1dc47e93c0149c1ffa12b3eb2840eab0
Wojciech Mosieński <wojciech.mosienski@ovoo.pl>
2026-08-08 19:12:21 +0200

    Remove lane warp and restore procedural trees

--- scripts/driving/scroll_spawn_handler.gd
+++ scripts/driving/scroll_spawn_handler.gd
@@ -19,10 +19,6 @@ const ScrollElementScript := preload("res://scripts/driving/scroll_element_2d.gd
 @export_group("")
 @export_range(0.001, 0.25, 0.001) var flat_road_length := 0.022
 @export_range(0.01, 1.0, 0.01) var flat_half_width_in_slots := 0.14
-@export_group("Lane Warp Shader")
-@export var use_lane_warp_shader := false
-@export_range(0.0, 0.8, 0.005) var lane_warp_strength := 0.14
-@export_group("")
 @export_range(0.05, 30.0, 0.01) var spawn_interval_seconds := 1.0
 @export var derive_interval_from_road_spacing := false
 @export_range(0.001, 1.0, 0.001) var road_spacing := 0.08
@@ -106,8 +102,6 @@ func _spawn(road_distance: float) -> void:
 	element.render_order_offset = render_order_offset
 	element.flat_road_length = flat_road_length
 	element.flat_half_width_in_slots = flat_half_width_in_slots
-	element.use_lane_warp_shader = use_lane_warp_shader
-	element.lane_warp_strength = lane_warp_strength
 	element.road_distance = road_distance
 	add_child(element)
 	_manager.apply_projection(element)
