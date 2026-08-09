7e0a173a2eb4dc994e412f9d1fb2adc2e10b0bd7
Adam Owczarek <adam.piotr.khan@gmail.com>
2026-08-09 09:55:51 +0200

    Bugfix

--- scripts/ui/multiscore.gd
+++ scripts/ui/multiscore.gd
@@ -1,5 +1,4 @@
 extends Sprite2D # (Change this to TextureRect if you are using a UI node)
-
 @export_group("Multiplier Textures")
 @export var texture_x1: Texture2D
 @export var texture_x2: Texture2D
@@ -7,9 +6,9 @@ extends Sprite2D # (Change this to TextureRect if you are using a UI node)
 @export var texture_x4: Texture2D
 @export var texture_x5: Texture2D
 @onready var keep_score_node = get_parent()
-
 var current_multiplier: int = 1
-	
+var _pulse_tween: Tween
+
 func _process(_delta):
 	if keep_score_node:
 		
@@ -22,7 +21,6 @@ func _process(_delta):
 			
 			current_multiplier = new_multiplier
 			update_graphic(current_multiplier)
-
 func update_graphic(mult: int):
 	
 	match mult:
@@ -41,3 +39,18 @@ func update_graphic(mult: int):
 		5:
 			texture = texture_x5
 			print("set texture5")
+	
+	_pulse_scale()
+
+func _pulse_scale():
+	# Cancel any pulse still in progress so rapid multiplier changes
+	# don't stack up competing tweens.
+	if _pulse_tween and _pulse_tween.is_valid():
+		_pulse_tween.kill()
+	
+	_pulse_tween = create_tween()
+	_pulse_tween.tween_property(self, "scale", Vector2(1.5, 1.5), 0.15) \
+		.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	_pulse_tween.tween_interval(0.3)
+	_pulse_tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.15) \
+		.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
